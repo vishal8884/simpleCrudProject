cbb3dec59f8704dd1025b10860464dd428053824
vishal <vishalambekar459@gmail.com>
2022-07-16 23:20:06 +0530

    Service annotation added

--- SimpleCRUDProject/src/main/java/io/crud/service/EmployeeServiceImpl.java
+++ SimpleCRUDProject/src/main/java/io/crud/service/EmployeeServiceImpl.java
@@ -6,10 +6,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import io.crud.model.Employee;
 import io.crud.repository.EmployeeRepository;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
 	
 	
